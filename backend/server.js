d93e53e9dd790f0cc8bcffcb2763c7a883b6ef6a
kbir079 <116657420+kbir-dev@users.noreply.github.com>
2025-02-15 22:49:31 +0530

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const dotenv = require('dotenv')
+const connectDB = require('./db/database')
+dotenv.config()
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
